fd55466a1ac747437152c3bbe8d0d6ffb14f1af4
Andrew Gray <andrew.aj.gray@gmail.com>
2020-08-18 20:24:30 +0930

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Requiring necessary npm packages
-const express = require('express');
+const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
