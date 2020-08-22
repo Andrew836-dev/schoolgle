5744e3907201f8317acf3db4e10a5d515af5d6c4
Andrew Gray <andrew.aj.gray@gmail.com>
2020-08-22 11:21:14 +0930

    new user model

--- models/user.js
+++ models/user.js
@@ -40,6 +40,10 @@ module.exports = function(sequelize, DataTypes) {
     postcode: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    roleID: {
+      type: DataTypes.INTEGER,
+      defaultValue: 200
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
