23328266658c158542bbc0c0cf771d0217ea461b
Andrew Gray <andrew.aj.gray@gmail.com>
2020-08-30 19:51:59 +0930

    added 'nearby' search functionality, began moving modular functions to separate js file

--- public/js/map-functions.js
+++ public/js/map-functions.js
@@ -1,19 +1,60 @@
-// /* This code borrows from http://jsfiddle.net/salman/4mtyu/ */
+/* eslint-disable indent */
+// indent and prettier were conflicting on this file
+function getSchoolsByPostcode(postcode) {
+  const body = {
+    postcode: postcode,
+    schoolType: getTypeTerm(),
+    state: getStateTerm()
+  };
+  return $.post("/api/schools/", body);
+}
 
-// /*
-//  * declare map as a global variable
-//  */
-// let map;
-// let center = [-25.808678, 134.918921];
-// /*
-//  * declare array for storing marker objects
-//  */
-// const markerArray = [];
-// /*
-//  * object for storing search conditions
-//  */
-// const conditions = {};
+// schoolName here should be a string
+// conditions is an object of the search conditions
+function getSchoolsByName(schoolName) {
+  const conditions = {
+    schoolType: getTypeTerm(),
+    state: getStateTerm()
+  };
+  return $.post("/api/schools/name/" + schoolName, conditions);
+}
 
+// get the school type from the form and put it into sequelize format
+function getTypeTerm() {
+  switch ($("#school-type").val()) {
+    case null:
+      return ["Primary", "Secondary", "Combined", "Special"];
+    default:
+      return [$("#school-type").val()];
+  }
+}
+
+// get the state from the form and put it into sequelize format
+function getStateTerm() {
+  switch ($("#state")) {
+    default:
+      return ["SA"];
+  }
+}
+
+function accountInit() {
+  const schoolgleMarkers = [];
+  $("tr").each(function() {
+    if ($(this).children("td").length > 0) {
+      schoolgleMarkers.push({
+        latitude: parseFloat($(this).data("lat")),
+        longitude: parseFloat($(this).data("long")),
+        schoolName: $(this).children("td")[0].textContent,
+        yearRange: $(this).children("td")[2].textContent,
+        schoolSector: $(this).children("td")[3].textContent,
+        enrolmentsTotal: parseInt($(this).data("enrolments")),
+        enrolmentsFTE: parseFloat($(this).data("enrolmentsfte")),
+        teachingStaffFTE: parseFloat($(this).data("teachersfte"))
+      });
+    }
+  });
+  return schoolgleMarkers;
+}
 // /*
 //  * use google maps api built-in mechanism to attach dom events
 //  */
